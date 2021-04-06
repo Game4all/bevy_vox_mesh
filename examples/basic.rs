603ccd545b3195b52fc609ef2f814d545d476c7f
Lucas A <game4allyt@gmail.com>
2021-04-06 16:44:58 +0200

    Add example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,10 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::{
+        pipeline::{PipelineDescriptor, RenderPipeline},
+        shader::{ShaderStage, ShaderStages},
+    },
+};
 use bevy_vox_mesh::VoxMeshPlugin;
 
 const BASIC_COLOR_VERT: &str = r#"#version 450
@@ -25,17 +31,64 @@ const BASIC_COLOR_FRAG: &str = r#"#version 450
 layout(location = 0) in vec4 vertex_color;
 layout(location = 0) out vec4 o_Target;
 
+vec4 toLinear(vec4 sRGB)
+{
+    bvec4 cutoff = lessThan(sRGB, vec4(0.04045));
+    vec4 higher = pow((sRGB + vec4(0.055))/vec4(1.055), vec4(2.4));
+    vec4 lower = sRGB/vec4(12.92);
+
+    return mix(higher, lower, cutoff);
+}
+
 void main() {
-    o_Target = vertex_color;
+    o_Target = toLinear(vertex_color);
 }"#;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(VoxMeshPlugin)
+        .add_startup_system(setup.system())
         .run();
 }
 
-fn setup() {
+fn setup(
+    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
+    mut shaders: ResMut<Assets<Shader>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    assets: ResMut<AssetServer>,
+) {
+    let handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
+        vertex: shaders.add(Shader::from_glsl(ShaderStage::Vertex, BASIC_COLOR_VERT)),
+        fragment: Some(shaders.add(Shader::from_glsl(ShaderStage::Fragment, BASIC_COLOR_FRAG))),
+    }));
+
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+
+    let mesh = assets.load("../examples/chicken.vox");
+
+    commands.spawn_bundle(MeshBundle {
+        mesh: mesh,
+        render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(handle)]),
+        transform: Transform::from_scale((0.01, 0.01, 0.01).into()),
+        ..Default::default()
+    });
+
+    // light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
     
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
 }
--- src/loader.rs
+++ src/loader.rs
@@ -1,8 +1,10 @@
 use anyhow::{anyhow, Error};
-use bevy::asset::{AssetLoader, LoadContext};
+use bevy::asset::{AssetLoader, LoadContext, LoadedAsset};
 
 #[derive(Default)]
 pub struct VoxLoader;
+
+use crate::mesher::mesh_model;
 
 impl AssetLoader for VoxLoader {
     fn load<'a>(
@@ -17,7 +19,7 @@ impl AssetLoader for VoxLoader {
     }
 
     fn extensions(&self) -> &[&str] {
-        &[".vox"]
+        &["vox"]
     }
 }
 
@@ -35,6 +37,17 @@ fn load_magica_voxel_file<'a>(
         .iter()
         .map(|color| color.to_le_bytes())
         .collect();
+
+    for (index, model) in file.models.iter().enumerate() {
+        let mesh = mesh_model(model, &palette);
+
+        match index {
+            0 => load_context.set_default_asset(LoadedAsset::new(mesh)),
+            _ => {
+                load_context.set_labeled_asset(&format!("Model{}", index), LoadedAsset::new(mesh));
+            }
+        }
+    }
 
     Ok(())
 }
